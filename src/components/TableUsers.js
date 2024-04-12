8f2ed72e778a69b00159c0f71a4737c829407d62
Lê Phúc Khương <khuonglp_tts@rikkeisoft.com>
2024-04-12 11:40:15 +0700

    april12

---
+++ src/components/TableUsers.js
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import { fetchAllUser } from '../services/UserService';
+import { useState } from 'react';
+
+const TableUsers = (props) => {
+
+    const [listUsers, setListUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = async () => {
+        let res = await fetchAllUser();
+        console.log('>>> New res ', res)
+        if (res && res.data) {
+            setListUsers(res.data);
+        }
+    }
+
+    console.log(listUsers)
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listUsers && listUsers.length > 0 &&
+                    listUsers.map((item, index) => {
+                        return (
+                            <tr key={`users-${index}`}>
+                                <td>{item.id}</td>
+                                <td>{item.email}</td>
+                                <td>{item.first_name}</td>
+                                <td>{item.last_name}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </Table>
+    );
+}
+export default TableUsers;
