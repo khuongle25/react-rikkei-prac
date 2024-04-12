25809c31b815813ddc58b85e823ea15906cf59a3
Lê Phúc Khương <khuonglp_tts@rikkeisoft.com>
2024-04-12 17:40:45 +0700

    up2

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -2,49 +2,79 @@ import { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
 import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+// import handlePageClick from './handlePageClick';
 
 const TableUsers = (props) => {
 
     const [listUsers, setListUsers] = useState([]);
 
+    const [totalUsers, setTotalUsers] = useState(0);
+
+    const [totalPages, setTotalPages] = useState(0);
+
     useEffect(() => {
-        getUsers();
+        getUsers(2);
     }, [])
 
-    const getUsers = async () => {
-        let res = await fetchAllUser();
-        console.log('>>> New res ', res)
+    const getUsers = async (page) => {
+        let res = await fetchAllUser(page);
         if (res && res.data) {
+            setTotalUsers(res.total);
             setListUsers(res.data);
+            setTotalPages(res.total_pages);
         }
     }
 
-    console.log(listUsers)
+    const handlePageClick = (event) => {
+        getUsers(+event.selected + 1);
+    }
+
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Email</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {listUsers && listUsers.length > 0 &&
-                    listUsers.map((item, index) => {
-                        return (
-                            <tr key={`users-${index}`}>
-                                <td>{item.id}</td>
-                                <td>{item.email}</td>
-                                <td>{item.first_name}</td>
-                                <td>{item.last_name}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </Table>
-    );
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                <tr key={`users-${index}`}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.first_name}</td>
+                                    <td>{item.last_name}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                pageClassName='page-item'
+                pageLinkClassName='page-link'
+                previousClassName='page-item'
+                previousLinkClassName='page-link'
+                nextClassName='page-item'
+                nextLinkClassName='page-link'
+                breakClassName='page-item'
+                breakLinkClassName='page-link'
+                containerClassName='pagination'
+                activeClassName='active'
+            />
+        </>
+    )
 }
 export default TableUsers;
